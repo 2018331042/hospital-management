516b5efe6f912fc4a4ddae256bdd9d81ce3228d9
Fahim <fahim@pop-os.localdomain>
2022-03-23 01:12:45 +0600

    practice Day -1

---
+++ pages/home.js
@@ -0,0 +1,50 @@
+
+// const dept = [
+//     {
+//         id:1,
+//         name:'oncology',
+//         description:'blah blah'
+//     },
+//     {
+//         id:2,
+//         name:'oncology',
+//         description:'blah blah'
+//     },
+//     {
+//         id:2,
+//         name:'oncology',
+//         description:'blah blah'
+//     }
+// ]
+
+
+function Home(props){
+    const {data} = props;
+    return(
+        <>
+            {
+                data.map((data) => (
+                    <div key={data.id}>
+                        <p>{data.id}</p>
+                        <p>{data.title}</p>
+                        <p>{data.body}</p>
+                    </div>
+                ))
+            }
+        </>
+       
+    )
+}
+
+export async function getServerSideProps(){
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const data = await response.json();
+    console.log({data});
+    return{
+        props:{
+            data,
+        }
+    }
+}
+
+export default Home;
