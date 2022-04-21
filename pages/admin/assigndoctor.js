d8e8ca607edda3e5592fac3ad7fbe4e87adf1342
Fahim <fahim@pop-os.localdomain>
2022-04-22 00:33:21 +0600

    assignDoctor component added

--- pages/admin/assigndoctor.js
+++ pages/admin/assigndoctor.js
@@ -1,7 +1,47 @@
-import React from 'react'
+import { Button, Grid, TextField, Typography } from '@mui/material';
+import React, { useState } from 'react';
 
 export default function AssignDoctor() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  console.log('here');
+  const submitHandler = () => {
+    console.log({email, password});
+  }
   return (
-    <div>assigndoctor</div>
+
+    <Grid container spacing={0} direction="column" alignItems="center" justifyContent="center" sx={{minHeight:'50vh'}}>
+         <Typography variant='h6' sx={{}}>
+          Assign Doctor
+        </Typography>
+      <Grid item md={12} lg={12}>
+      <TextField
+       id="email"
+       label="Email"
+       type="Email"
+       autoComplete="Email"
+       margin='normal'
+       variant="outlined"
+       onChange={(e) => setEmail(e.target.value)}
+     />
+    </Grid>
+    <Grid item >
+      <TextField
+       id="password"
+       label="Password"
+       type="password"
+       autoComplete="current-password"
+       variant="outlined"
+       margin='normal'
+       onChange={(e) => setPassword(e.target.value)}
+     />
+    </Grid>
+     <Button variant='contained' color="secondary" onClick={submitHandler}>
+       Submit
+     </Button>
+     
+    </Grid>
+
+
   )
 }
