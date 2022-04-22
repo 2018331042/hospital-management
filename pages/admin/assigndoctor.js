6dbd26d8a1695a64543baef18a5da15ffc54ef03
Arif Shahriar <arifshahriar028@gmail.com>
2022-04-23 00:23:24 +0600

    test commit

--- pages/admin/assigndoctor.js
+++ pages/admin/assigndoctor.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import Box from "@mui/material/Box";
 
 export default function AssignDoctor() {
   return (
-    <div>assigndoctor</div>
-  )
+    <div>
+      <Box
+        sx={{
+          width: {
+            xs: 100, // theme.breakpoints.up('xs')
+            sm: 200, // theme.breakpoints.up('sm')
+            md: 300, // theme.breakpoints.up('md')
+            lg: 400, // theme.breakpoints.up('lg')
+            xl: 500, // theme.breakpoints.up('xl')
+          },
+        }}
+      >
+        This box has a responsive width.
+      </Box>
+    </div>
+  );
 }
--- pages/admin/createdepartment.js
+++ pages/admin/createdepartment.js
@@ -1,7 +1,20 @@
-import React from 'react'
+import React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 
 export default function CreateDepartment() {
   return (
-    <div>createdepartment</div>
+    <Box
+      component="form"
+      sx={{
+        '& > :not(style)': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+
+      
+      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+    </Box>
   )
 }
