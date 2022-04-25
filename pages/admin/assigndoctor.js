55eda30c1ef83eea24d31dd8a42d4697579989e1
Arif Shahriar <arifshahriar028@gmail.com>
2022-04-25 22:40:44 +0600

    assigndoctor and creatdepartment modified

--- pages/admin/assigndoctor.js
+++ pages/admin/assigndoctor.js
@@ -1,21 +1,72 @@
-import Box from "@mui/material/Box";
+import { Box, Button, Grid, Paper, TextField, Typography } from "@mui/material";
+import { useState } from "react";
+import Appbar from "../../components/NavBar";
 
 export default function AssignDoctor() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  console.log("here");
+  const submitHandler = () => {
+    console.log({ email, password });
+  };
   return (
     <div>
-      <Box
-        sx={{
-          width: {
-            xs: 100, // theme.breakpoints.up('xs')
-            sm: 200, // theme.breakpoints.up('sm')
-            md: 300, // theme.breakpoints.up('md')
-            lg: 400, // theme.breakpoints.up('lg')
-            xl: 500, // theme.breakpoints.up('xl')
-          },
-        }}
+      <Appbar />
+      <Grid
+        container
+        spacing={0}
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+        sx={{ minHeight: "50vh", marginTop: "10vh" }}       
       >
-        This box has a responsive width.
-      </Box>
+        <Typography variant="h5" component="h1" sx={{ color: "Grey", fontWeight: 'bold' }}>
+          Assign Doctor
+        </Typography>
+
+        <Paper elevation={3} sx={{ margin: 2 }}>
+          <Box sx={{ padding: 1, margin: 10 }}>
+          <Grid item md={12} lg={12}>
+              <TextField
+                id="name" // id is required
+                label="Name"
+                type="text"
+                autoComplete="Doctor Name"
+                variant="outlined"
+                margin="normal"
+                sx={{ width: "300px" }}
+              />
+            </Grid>
+            <Grid item md={12} lg={12}>
+              <TextField
+                id="email" // id is required
+                label="Email"
+                type="Email"
+                autoComplete="Email"
+                variant="outlined"
+                margin="normal"
+                sx={{ width: "300px" }}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </Grid>
+            <Grid item>
+              <TextField
+                id="password"
+                label="Password"
+                type="password"
+                autoComplete="current-password"
+                variant="outlined"
+                margin="normal"
+                sx={{ width: "300px" }}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </Grid>
+          </Box>
+        </Paper>
+        <Button variant="contained" color="secondary" onClick={submitHandler}>
+          Submit
+        </Button>
+      </Grid>
     </div>
   );
 }
--- pages/admin/createdepartment.js
+++ pages/admin/createdepartment.js
@@ -1,20 +1,59 @@
-import React from 'react';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
+import { Grid, Typography } from "@mui/material";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Appbar from "../../components/NavBar";
 
 export default function CreateDepartment() {
   return (
-    <Box
-      component="form"
-      sx={{
-        '& > :not(style)': { m: 1, width: '25ch' },
-      }}
-      noValidate
-      autoComplete="off"
-    >
-
-      
-      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-    </Box>
-  )
+    <>
+      <Appbar />
+      <Grid
+        container
+        spacing={2}
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+        sx={{ marginTop: "10vh" }}
+      >
+        <Typography
+          variant="h5"
+          component="h1"
+          sx={{ color: "Grey", fontWeight: "bold" }}
+        >
+          Create Department
+        </Typography>
+        <Paper elevation={3} sx={{ margin: 2 }}>
+          <Box sx={{ margin: 10 }}>
+            <Grid item md={12} lg={12}>
+              <TextField
+                id="dept_id" // id is required
+                label="Department Name"
+                type="text"
+                autoComplete="Department Name"
+                variant="outlined"
+                margin="normal"
+                sx={{ width: "300px" }}
+              />
+            </Grid>
+            <Grid item md={12} lg={12}>
+              <TextField
+                id="dept_code" // id is required
+                label="Department Code"
+                type="number"
+                autoComplete="Department Code"
+                variant="outlined"
+                margin="normal"
+                sx={{ width: "300px" }}
+              />
+            </Grid>
+          </Box>
+        </Paper>
+        <Button variant="contained" color="secondary">
+          Submit
+        </Button>
+      </Grid>
+    </>
+  );
 }
