06d891faa7a38a2d162da591ea2c14e8eb3775f3
Arif Shahriar <arifshahriar028@gmail.com>
2022-07-21 20:25:03 +0600

    some changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,9 @@
-import { NotificationsProvider } from "@mantine/notifications";
-import { Sidebar } from "../components/sidebar";
-import "../styles/globals.css";
-import { AuthProvider } from "../utils/contexts/auth";
-import { SidebarProvider } from "../utils/contexts/sidebarContext";
-import "../styles/patient.profile.css";
+import { NotificationsProvider } from '@mantine/notifications';
+import { Sidebar } from '../components/sidebar';
+import '../styles/globals.css';
+import { AuthProvider } from '../utils/contexts/auth';
+import { SidebarProvider } from '../utils/contexts/sidebarContext';
+import '../styles/patient.profile.css';
 function MyApp({ Component, pageProps }) {
   return (
     <NotificationsProvider>
